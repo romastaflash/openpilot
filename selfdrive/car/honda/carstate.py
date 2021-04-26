68d18dd9b546e05e52ace89fc73431eb9d327197
Nightrider46 <78983073+Nightrider46@users.noreply.github.com>
2021-04-25 20:20:27 -0400

    Attempt to fix brake pedal noise

--- selfdrive/car/honda/carstate.py
+++ selfdrive/car/honda/carstate.py
@@ -326,7 +326,7 @@ class CarState(CarStateBase):
     ret.cruiseState.nonAdaptive = self.cruise_mode != 0
 
     # Gets rid of Pedal Grinding noise when brake is pressed at slow speeds for some models
-    if self.CP.carFingerprint in (CAR.PILOT, CAR.PILOT_2019, CAR.RIDGELINE):
+    if self.CP.carFingerprint in (CAR.PILOT, CAR.CIVIC, CAR.PILOT_2019, CAR.RIDGELINE):
       if ret.brake > 0.05:
         ret.brakePressed = True
 
