afb2be04d075181ba42c3f01c3aeb720b824d013
Nightrider46 <toms0760@gmail.com>
2021-04-28 19:04:19 -0400

    Accel Test

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -21,13 +21,13 @@ AWARENESS_DECEL = -0.2     # car smoothly decel at .2m/s^2 when user is distract
 
 # lookup tables VS speed to determine min and max accels in cruise
 # make sure these accelerations are smaller than mpc limits
-_A_CRUISE_MIN_V = [-3.0, -2.6, -2.3, -2.0, -1.0]
+_A_CRUISE_MIN_V = [-2.0, -1.5, -1.0, -0.7, -0.5]
 # _A_CRUISE_MIN_V = [-1.15, -.85, -.7, -.55, -.32]
 _A_CRUISE_MIN_BP = [0.0, 5.0, 10.0, 20.0, 55.0]
 
 # need fast accel at very low speed for stop and go
 # make sure these accelerations are smaller than mpc limits
-_A_CRUISE_MAX_V = [3.0, 3.5, 3.0, 2.0, 2.0]
+_A_CRUISE_MAX_V = [2.0, 2.0, 1.5, .5, .3]
 _A_CRUISE_MAX_V_FOLLOWING = [1.6, 1.4, 1.4, .7, .3]
 # _A_CRUISE_MAX_V = [1.6, 1.4, 0.7, .4]
 # _A_CRUISE_MAX_V_FOLLOWING = [1.7, 1.65, 0.7, .5]
