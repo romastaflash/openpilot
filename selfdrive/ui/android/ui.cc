fb61217a84a280e5f4c19b9bd142c7581e2e5df2
Nightrider46 <toms0760@gmail.com>
2021-05-09 18:11:57 -0400

    UI Testing

--- selfdrive/ui/android/ui.cc
+++ selfdrive/ui/android/ui.cc
@@ -64,8 +64,9 @@ static bool handle_ls_touch(UIState *s, int touch_x, int touch_y) {
 
 static bool handle_df_touch(UIState *s, int touch_x, int touch_y) {
   //dfButton manager
+  int padding = 40;
   int btn_x_1 = 1660 - 200;
-  int btn_x_2 = 1660 - -10;
+  int btn_x_2 = 1660 - 50;
   if ((btn_x_1 - padding <= touch_x) && (touch_x <= btn_x_2 + padding) && (855 - padding <= touch_y)) {
     s->scene.dfButtonStatus++;
     if (s->scene.dfButtonStatus > 3) { s->scene.dfButtonStatus = 0; }
