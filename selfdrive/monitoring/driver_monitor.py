2f59a59948c3cc3da72c93dde22b346ee32a4569
romastaflash <68671537+romastaflash@users.noreply.github.com>
2021-10-25 06:56:22 -0400

    Update driver_monitor.py
        self._AWARENESS_TIME = 99999. # passive wheeltouch total timeout  [previously 35]

--- selfdrive/monitoring/driver_monitor.py
+++ selfdrive/monitoring/driver_monitor.py
@@ -18,7 +18,7 @@ EventName = car.CarEvent.EventName
 class DRIVER_MONITOR_SETTINGS():
   def __init__(self, TICI=TICI, DT_DMON=DT_DMON):
     self._DT_DMON = DT_DMON
-    self._AWARENESS_TIME = 35. # passive wheeltouch total timeout
+    self._AWARENESS_TIME = 99999. # passive wheeltouch total timeout  [previously 35]
     self._AWARENESS_PRE_TIME_TILL_TERMINAL = 12.
     self._AWARENESS_PROMPT_TIME_TILL_TERMINAL = 6.
     self._DISTRACTED_TIME = 11. # active monitoring total timeout
