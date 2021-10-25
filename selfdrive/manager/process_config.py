63944aafb8e5918079ecfa437fd1938ca8510dd2
romastaflash <68671537+romastaflash@users.noreply.github.com>
2021-10-25 06:57:14 -0400

    Update process_config.py
    # PythonProcess("uploader", "selfdrive.loggerd.uploader", persistent=True),

--- selfdrive/manager/process_config.py
+++ selfdrive/manager/process_config.py
@@ -34,7 +34,7 @@ procs = [
   PythonProcess("timezoned", "selfdrive.timezoned", enabled=TICI, persistent=True),
   PythonProcess("tombstoned", "selfdrive.tombstoned", enabled=not PC, persistent=True),
   PythonProcess("updated", "selfdrive.updated", enabled=not PC, persistent=True),
-  PythonProcess("uploader", "selfdrive.loggerd.uploader", persistent=True),
+# PythonProcess("uploader", "selfdrive.loggerd.uploader", persistent=True),
 
   # EON only
   PythonProcess("rtshield", "selfdrive.rtshield", enabled=EON),
